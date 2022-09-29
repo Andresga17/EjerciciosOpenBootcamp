489e9f634657611adaadf9cc4b4978d862f4deec
Andres1717 <mariaerq10@gmail.com>
2022-09-29 13:53:20 -0500

    Ejercicios - tema 1 Tipos de datos

---
+++ src/com/javabasico/TiposDeDatos.java
@@ -0,0 +1,40 @@
+package com.javabasico;
+
+public class TiposDeDatos {
+    public static void main(String[] args) {
+        //1. Tipos de datos numericos
+        //  1.1. Enteros
+        byte number1 = 73;
+        System.out.println(number1);
+
+        short number2 = 1729;
+        System.out.println(number2);
+
+        int number3 = 9143;
+        System.out.println(number3);
+
+        long number4 = 323343035;
+        System.out.println(number4);
+
+        //  1.2 numeros con decimales
+        float number5 = 7.34767309F;
+        System.out.println(number5);
+
+        double number6 = 6.626070151034D;
+        System.out.println(number6);
+
+        //2. Tipos de dato de texto
+        char letter = 'k';
+        System.out.println(letter);
+
+        String word = "Mi nombre es Andres";
+        System.out.println(word);
+
+        //  2.1 Tipo de dato booleano
+        boolean verdadero = true;
+        System.out.println(verdadero);
+        boolean falso = false;
+        System.out.println(falso);
+
+    }
+}
