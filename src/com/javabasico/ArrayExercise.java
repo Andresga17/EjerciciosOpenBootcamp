90e98d306c21bbe85bc50508985d1c9579ce3aa2
Andres1717 <mariaerq10@gmail.com>
2022-12-26 22:44:31 -0500

    Commit de funcion reverse

---
+++ src/com/javabasico/ArrayExercise.java
@@ -0,0 +1,21 @@
+package com.javabasico;
+
+/*
+Dada la funcion reverse Escribe el codigo que devuelva una cadena al revers. Por
+ejemplo, la cadena "hola mundo", debe retornar "odnum aloh"
+
+1. Crea un array unidemensional de Strings y recorrelo, mostrando unicamente sus valores.
+*/
+public class ArrayExercise {
+    //String texto = "Hola mundo";
+   public static String reverse (String texto){
+       char [] textoArray = texto.toCharArray();
+//       ArrayList <Character> arrayReverse = new ArrayList<>();
+       String reverseString = "";
+       for (int i = texto.length() - 1; i >= 0; i--) {
+//           arrayReverse.add(textoArray[i]);
+           reverseString += textoArray[i];
+       }
+       return reverseString;
+   }
+}
--- src/interfaces/Main.java
+++ src/interfaces/Main.java
@@ -1,12 +1,15 @@
 package interfaces;
 
+import com.javabasico.ArrayExercise;
+
 public class Main {
     public static void main(String[] args) {
 
         CocheCRUDImpl crud = new CocheCRUDImpl();
-        
-         crud.save(crud);
-         System.out.println(crud.findAll());
-         crud.delete(crud);
+
+        crud.save(crud);
+        System.out.println(crud.findAll());
+        crud.delete(crud);
+        System.out.println(ArrayExercise.reverse("Hola mundo"));
     }
 }
