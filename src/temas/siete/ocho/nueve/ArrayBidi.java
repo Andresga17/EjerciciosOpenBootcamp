58d61f40d4af06f490444071da6199a615018faf
Andres1717 <mariaerq10@gmail.com>
2022-12-30 13:59:14 -0500

    Ejercicio 2. Array bidimensional

---
+++ src/temas/siete/ocho/nueve/ArrayBidi.java
@@ -0,0 +1,20 @@
+/*
+* 2. Crea un array bidimensional de enteros y recórrelo, mostrando la posicion y el valor de cada elemento en ambas dimensiones.
+*/
+package temas.siete.ocho.nueve;
+
+public class ArrayBidi {
+    public static void main(String[] args){
+
+        int [][] numbers = new int[3][4];
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[0].length; j++){
+                numbers[0][j] += j + 1;
+                numbers[1][j] += j + 2;
+                numbers[2][j] += j + 3;
+                System.out.println("Posicion: " + i + j + " " + "Valor: " + numbers[i][j]);
+            }
+        }
+    }
+
+}
